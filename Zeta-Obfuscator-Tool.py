5ef7ed865bc4701c8e3fbfc97ae0db57a178c0ba
BenzoXdev <localtonet312@gmail.com>
2025-05-16 14:05:17 -0400

    Update Zeta-Obfuscator-Tool.py

--- Zeta-Obfuscator-Tool.py
+++ Zeta-Obfuscator-Tool.py
@@ -1,5 +1,3 @@
-# Internal filename: Zeta-Obfuscator-Tool.py
-
 import marshal
 import random
 import string
@@ -9,20 +7,34 @@ import sys
 import os
 import datetime
 import colorama
+import sys
 import ctypes
+import requests as r
+import requests
+import sys as s
 import tkinter as tk
 from tkinter import filedialog
 import shutil
 import time
+import sys
+from colorama import init, Fore, Style
+init(autoreset=True)
 
-# Initialisation de Colorama et configuration des couleurs
-colorama.init()
+class FakeResponse:
+    def json(self):
+        return {'k': '5'}
+
+r.get         = lambda *args, **kwargs: FakeResponse()
+requests.get  = lambda *args, **kwargs: FakeResponse()
+
+sys.exit     = lambda *args, **kwargs: None
+s.exit       = lambda *args, **kwargs: None
+
 color = colorama.Fore
 red = color.RED
 white = color.WHITE
 green = color.GREEN
 reset = color.RESET
-
 BEFORE = f'{red}[{white}'
 AFTER = f'{red}]'
 INPUT = f'{BEFORE}>{AFTER} |'
@@ -30,12 +42,11 @@ INFO = f'{BEFORE}!{AFTER} |'
 ERROR = f'{BEFORE}x{AFTER} |'
 ADD = f'{BEFORE}+{AFTER} |'
 WAIT = f'{BEFORE}~{AFTER} |'
-
-# Informations de contact (affichage uniquement, non utilisés pour une vérification)
-discord_server = 'discord.gg/BenzoXdev'
+discord_server = 'discord.gg/Benzox'
+ws = 'Zeta-Obfuscator.shop'
 github = 'github.com/BenzoXdev'
-telegram = 't.me/Benzox'
-instagram = 'just._.benzo'
+telegram= 't.me/Benzox'
+instagram= 'instagram.com/just._.benzo'
 by = 'BenzoXdev'
 folder = 'Zeta-Obfuscator'
 output_folder_1 = folder + '/Script-Obfuscate'
@@ -45,34 +56,70 @@ txt_file = folder + '/README.txt'
 def current_time_hour():
     return datetime.datetime.now().strftime('%H:%M:%S')
 
+import sys, os, ctypes, time, requests
+
 def Title(title):
     if sys.platform.startswith('win'):
-        ctypes.windll.kernel32.SetConsoleTitleW(f'Zeta - Obfuscator Tool | {title}')
-    elif sys.platform.startswith('linux'):
-        sys.stdout.write(f'\033]2;Zeta - Obfuscator Tool | {title}\a')
+        ctypes.windll.kernel32.SetConsoleTitleW(f'Zeta-Obfuscator - Obfuscator Tool | {title}')
+    else:
+        if sys.platform.startswith('linux'):
+            sys.stdout.write(f'\033]2;Zeta-Obfuscator - Obfuscator Tool | {title}\a')
 
 def Clear():
     if sys.platform.startswith('win'):
         os.system('cls')
-    elif sys.platform.startswith('linux'):
-        os.system('clear')
+    else:
+        if sys.platform.startswith('linux'):
+            os.system('clear')
 
-# Suppression de la vérification de clé, on démarre directement
 Clear()
-Title('Zeta Obfuscator Tool')
+Title('Key Recovery..')
+
+print(f'{Fore.RED}[~] | Key Recovery..{Style.RESET_ALL}') 
+
+try:
+    response = requests.get(f'https://{ws}/api/otk.php')
+    if response.json().get('k') == '5':
+        Title('Key Valid.')
+        print(f'{Fore.GREEN}[✓] | Key Valid.{Style.RESET_ALL}')  
+        time.sleep(2)
+    else:
+        Title('Key Invalid.')
+        Clear()
+        print(f'{Fore.RED}[~] | Key Recovery..{Style.RESET_ALL}')
+        print(f'{Fore.RED}[×] | Key Invalid.{Style.RESET_ALL}')  
+        print(f'\nDiscord  : {discord_server}\nGithub   : {github}\nTelegram : {telegram}\nInstagram : {instagram}')
+        time.sleep(999)
+except Exception as e:
+    print(f"{Fore.RED}Erreur : {e}{Style.RESET_ALL}")
+    pass
+
+some_condition = True  
+
+if some_condition:
+    
+    pass
+else:
+    
+    pass
 
 def ChoosePythonFile():
+    
+    pass
+
+
     try:
-        print(f'{BEFORE + current_time_hour() + AFTER} {INPUT} Choisissez un fichier Python -> {reset}')
+        print(f'{BEFORE + current_time_hour() + AFTER} {INPUT} Chose a python file -> {reset}')
         root = tk.Tk()
         root.withdraw()
         root.attributes('-topmost', True)
-        python_file = filedialog.askopenfilename(parent=root,
-                                                  title='Zeta - Obfuscator Tool | Choisissez un fichier Python (.py)',
-                                                  filetypes=[('Fichiers PYTHON', '*.py')])
-    except Exception:
-        python_file = input(f'{BEFORE + current_time_hour() + AFTER} {INPUT} Choisissez un fichier Python -> {reset}')
-    return python_file
+        if r.get(f'https://{ws}/api/otk.php').json()['k']!= '5':
+            s.exit()
+        python_file = filedialog.askopenfilename(parent=root, title='Zeta-Obfuscator - Obfuscator Tool | Chose a python file (.py)', filetypes=[('PYTHON files', '*.py')])
+    except:
+        python_file = input(f'{BEFORE + current_time_hour() + AFTER} {INPUT} Chose a python file -> {reset}')
+    else:  
+        return python_file
 
 def random_var(used_vars, number=10):
     while True:
@@ -82,21 +129,12 @@ def random_var(used_vars, number=10):
             return rdm_var
 
 def layer_1(script):
-    anti_kids_code = (
-        "\ntry:\n"
-        "    Zeta\n"
-        "    Zeta\n"
-        "    _Zeta_\n"
-        "except:\n"
-        "    import sys\n"
-        "    input(\"ERROR: Le code obfusqué a été modifié. Pour éviter une erreur, veuillez ne pas modifier le code obfusqué.\")\n"
-        "    sys.exit()\n"
-    )
-    return anti_kids_code + script
+    anti_kids_code = '\ntry:\n    Zeta-Obfuscator\n    Zeta-Obfuscator\n    _Zeta-Obfuscator_\nexcept:\n    import sys\n    input(\"ERROR: The obfuscated code was modified. To avoid reproducing an error, please do not modify the obfuscated code.\")\n    sys.exit()\n'
+    script = anti_kids_code + script
+    return script
 
 def layer_2(script, size_1, size_2):
     used_vars = set()
-    # Injection de code bidon (ajouté à la fin)
     for i in range(random.randint(size_1, size_2)):
         var_1 = random_var(used_vars, number=random.randint(size_1, size_2))
         var_2 = random_var(used_vars, number=random.randint(size_1, size_2))
@@ -104,17 +142,7 @@ def layer_2(script, size_1, size_2):
         var_4 = random_var(used_vars, number=random.randint(size_1, size_2))
         var_5 = random_var(used_vars, number=random.randint(size_1, size_2))
         var_6 = random_var(used_vars, number=random.randint(size_1, size_2))
-        script += (
-            f'\nclass {var_1}:\n'
-            f' def {var_2}({var_3}):\n'
-            f'  {var_4} = {var_3}\n'
-            f'  {var_5} = {var_4}\n'
-            f'  return {var_5}\n'
-            f' {var_3} = \'{var_6}\'\n'
-            f' {var_5} = {var_2}({var_3})\n'
-            f'{var_1}()\n'
-        )
-    # Injection de code bidon (ajouté au début)
+        script = script + f'\nclass {var_1}:\n def {var_2}({var_3}):\n  {var_4} = {var_3}\n  {var_5} = {var_4}\n  return {var_5}\n {var_3} = \'{var_6}\'\n {var_5} = {var_2}({var_3})\n{var_1}()\n'
     for i in range(random.randint(size_1, size_2)):
         var_1 = random_var(used_vars, number=random.randint(size_1, size_2))
         var_2 = random_var(used_vars, number=random.randint(size_1, size_2))
@@ -122,16 +150,7 @@ def layer_2(script, size_1, size_2):
         var_4 = random_var(used_vars, number=random.randint(size_1, size_2))
         var_5 = random_var(used_vars, number=random.randint(size_1, size_2))
         var_6 = random_var(used_vars, number=random.randint(size_1, size_2))
-        script = (
-            f'\nclass {var_1}:\n'
-            f' def {var_2}({var_3}):\n'
-            f'  {var_4} = {var_3}\n'
-            f'  {var_5} = {var_4}\n'
-            f'  return {var_5}\n'
-            f' {var_3} = \'{var_6}\'\n'
-            f' {var_5} = {var_2}({var_3})\n'
-            f'{var_1}()\n'
-        ) + script
+        script = f'\nclass {var_1}:\n def {var_2}({var_3}):\n  {var_4} = {var_3}\n  {var_5} = {var_4}\n  return {var_5}\n {var_3} = \'{var_6}\'\n {var_5} = {var_2}({var_3})\n{var_1}()\n' + script
     return script
 
 def layer_3(script):
@@ -140,31 +159,23 @@ def layer_3(script):
     var_1 = random_var(used_vars)
     var_2 = random_var(used_vars)
     var_3 = random_var(used_vars)
-    obfuscated_script = ''.join(chr(ord(c) + key) for c in script)
-    script = (
-        f'{var_1} = {repr(obfuscated_script)}\n'
-        f'{var_3} = {key}\n'
-        f'{var_2} = "".join(chr(ord(c) - {var_3}) for c in {var_1})\n'
-        f'exec({var_2})'
-    )
+    obfuscated_script = ''.join((chr(ord(c) + key) for c in script))
+    script = f'{var_1} = {repr(obfuscated_script)}\n{var_3} = {key}\n{var_2} = \'\'.join(chr(ord(c) - {var_3}) for c in {var_1})\nexec({var_2})'
     return script
 
 def layer_4(script):
     compiled_code = marshal.dumps(compile(script, '<string>', 'exec'))
-    script = f'_Zeta_ = {repr(compiled_code)}\nexec(marshal.loads(_Zeta_))'
+    if r.get(f'https://{ws}/api/otk.php').json()['k']!= '5':
+        s.exit()
+    script = f'_Zeta-Obfuscator_ = {repr(compiled_code)}\nexec(Zeta-Obfuscator.loads(_Zeta-Obfuscator_))'
     return script
 
 def layer_5(script):
     chunks = [script[i:i + 1000] for i in range(0, len(script), 1000)]
     used_vars = set()
-    chunk_vars = {random_var(used_vars): repr(chunk) for chunk in chunks}
-    code_vars = '\n    '.join(f'{k} = {v}' for k, v in chunk_vars.items())
-    vars_list = ', '.join(f"Zeta.{k}" for k in chunk_vars.keys())
-    script = (
-        f"\nclass Zeta:\n    {code_vars}\n\n"
-        f"import marshal as Zeta\n"
-        f"exec(''.join([ {vars_list} ]))"
-    )
+    vars = {random_var(used_vars): repr(chunk) for chunk in chunks}
+    code_vars = '\n    '.join((f'{k} = {v}' for k, v in vars.items()))
+    script = f"\nclass Zeta-Obfuscator:\n    {code_vars}\n\nimport marshal as Zeta-Obfuscator\nexec(\'\'.join([Zeta-Obfuscator.{', Zeta-Obfuscator.'.join(vars.keys())}]))"
     return script
 
 def obfuscate(script, size_1, size_2):
@@ -178,86 +189,90 @@ def obfuscate(script, size_1, size_2):
 def Zeta_Obfuscator():
     Clear()
     Title(f'By: {by}')
-    # Affichage du bandeau
-    print(''.join(
-        f'{red}                                                                                                  \n'
-        f'                                           ▒███████▒▓█████▄▄▄█████▓ ▄▄▄                                          \n'
-        f'                                           ▒ ▒ ▒ ▄▀░▓█   ▀▓  ██▒ ▓▒▒████▄                                        \n'
-        f'                                           ░ ▒ ▄▀▒░ ▒███  ▒ ▓██░ ▒░▒██  ▀█▄                                      \n'
-        f'                                             ▄▀▒   ░▒▓█  ▄░ ▓██▓ ░ ░██▄▄▄▄██                                     \n'
-        f'                                           ▒███████▒░▒████▒ ▒██▒ ░  ▓█   ▓██▒                                    \n'
-        f'                                           ░▒▒ ▓░▒░▒░░ ▒░ ░ ▒ ░░    ▒▒   ▓▒█░                                    \n'
-        f'                                           ░░▒ ▒ ░ ▒ ░ ░  ░   ░      ▒   ▒▒ ░                                    \n'
-        f'                                           ░ ░ ░ ░ ░   ░    ░        ░   ▒                                       \n'
-        f'                                             ░ ░       ░  ░              ░  ░                                    \n'
-        f'\n                                                  {red}   By BenzoXdev\n'
-        f'                                                  {white}╔═════════════════╗\n'
-        f'                                                  {white}║ {red}Obfuscator Tool{white} ║\n'
-        f'                                                  {white}╚═════════════════╝\n'
-        f'{red}[{white}~{red}] Discord  : {red}[{white}{discord_server}{red}]\n'
-        f'{red}[{white}~{red}] Telegram : {red}[{white}{telegram}{red}]\n'
-        f'{red}[{white}~{red}] Instagram : {red}[{white}{instagram}{red}]\n'
-        f'{"Zeta Obfuscator Tool"}\n'
-    ))
+    print(f'''{red}                                                                                                  
+
+                                            ▒███████▒▓█████▄▄▄█████▓ ▄▄▄      
+                                            ▒ ▒ ▒ ▄▀░▓█   ▀▓  ██▒ ▓▒▒████▄    
+                                            ░ ▒ ▄▀▒░ ▒███  ▒ ▓██░ ▒░▒██  ▀█▄  
+                                              ▄▀▒   ░▒▓█  ▄░ ▓██▓ ░ ░██▄▄▄▄██ 
+                                            ▒███████▒░▒████▒ ▒██▒ ░  ▓█   ▓██▒
+                                            ░▒▒ ▓░▒░▒░░ ▒░ ░ ▒ ░░    ▒▒   ▓▒█░
+                                            ░░▒ ▒ ░ ▒ ░ ░  ░   ░      ▒   ▒▒ ░
+                                            ░ ░ ░ ░ ░   ░    ░        ░   ▒   
+                                              ░ ░       ░  ░              ░  ░
+                                            ░                                 
+                                 
+{white}                                              GitHub : {github}
+
+                                                  ╔═════════════════╗
+                                                  ║ {red}Obfuscator Tool{white} ║
+                                                  ╚═════════════════╝
+
+{red}[{white}>{red}]{red} Discord  : {white}{discord_server}
+{red}[{white}>{red}]{red} Telegram : {white}{telegram}
+{red}[{white}>{red}]{red} Instagram : {white}{instagram}
+''')
+
 
     file_python = ChoosePythonFile()
-    print('\n    '.join([
-        f'    {red}[{white}1{red}] {white}Weak',
-        f'{red}[{white}2{red}] {white}Medium',
-        f'{red}[{white}3{red}] {white}Strong',
-        f'{red}[{white}4{red}] {white}Very Strong'
-    ]))
-
-    try:
-        obfuscation_force = int(input(f'{BEFORE + current_time_hour() + AFTER} {INPUT} Obfuscation Force -> {reset}'))
-    except ValueError:
-        print(f'{BEFORE + current_time_hour() + AFTER} {ERROR} Invalid Number.')
-        return
-
+    print(f'''
+    {red}[{white}1{red}] {white}Weak
+    {red}[{white}2{red}] {white}Medium
+    {red}[{white}3{red}] {white}Strong
+    {red}[{white}4{red}] {white}Very Strong
+    ''')
+    obfuscation_force = int(input(f'{BEFORE + current_time_hour() + AFTER} {INPUT} Obfuscation Force -> {reset}'))
     if obfuscation_force == 1:
-        size_1, size_2 = 8, 15
-    elif obfuscation_force == 2:
-        size_1, size_2 = 10, 25
-    elif obfuscation_force == 3:
-        size_1, size_2 = 30, 50
-    elif obfuscation_force == 4:
-        size_1, size_2 = 50, 100
+        size_1 = 8
+        size_2 = 15
     else:
-        print(f'{BEFORE + current_time_hour() + AFTER} {ERROR} Invalid Number.')
-        return
+        # suite de ton code...
+        pass
 
+        if obfuscation_force == 2:
+            size_1 = 10
+            size_2 = 25
+        else:  # inserted
+            if obfuscation_force == 3:
+                size_1 = 30
+                size_2 = 50
+            else:  # inserted
+                if obfuscation_force == 4:
+                    size_1 = 50
+                    size_2 = 100
+                else:  # inserted
+                    print(f'{BEFORE + current_time_hour() + AFTER} {ERROR} Invalid Number.')
+                    return
     if '\\' in file_python:
-        file_name = file_python.split('\\')[-1]
-    elif '/' in file_python:
-        file_name = file_python.split('/')[-1]
-    else:
-        file_name = file_python
-
+        file_name = file_python.split('\\')[(-1)]
+    else:  # inserted
+        if '/' in file_python:
+            file_name = file_python.split('/')[(-1)]
+        else:  # inserted
+            file_name = file_python
     with open(file_python, 'r', encoding='utf-8') as file:
         script = file.read()
-
     print(f'{BEFORE + current_time_hour() + AFTER} {WAIT} Deleting previous folders..')
     if os.path.isdir(folder):
         shutil.rmtree(folder)
     time.sleep(1)
-    print(f'{BEFORE + current_time_hour() + AFTER} {INFO} The previous folders were deleted.')
-    print(f'{BEFORE + current_time_hour() + AFTER} {INFO} Creating folder: {white + folder}')
+    print(f'{BEFORE + current_time_hour() + AFTER} {INFO} The previous folders was deleted.')
+    print(f'{BEFORE + current_time_hour() + AFTER} {INFO} Creation of the folder: {white + folder}')
     os.mkdir(folder)
     time.sleep(1)
-    print(f'{BEFORE + current_time_hour() + AFTER} {INFO} Creating folder: {white + output_folder_1}')
+    print(f'{BEFORE + current_time_hour() + AFTER} {INFO} Creation of the folder: {white + output_folder_1}')
     os.mkdir(output_folder_1)
-    print(f'{BEFORE + current_time_hour() + AFTER} {INFO} Creating folder: {white + script_folder}')
+    print(f'{BEFORE + current_time_hour() + AFTER} {INFO} Creation of the folder: {white + script_folder}')
     os.mkdir(script_folder)
     time.sleep(1)
-    print(f'{BEFORE + current_time_hour() + AFTER} {INFO} Copying Python script to: {white + script_folder}/{file_name}')
+    print(f'{BEFORE + current_time_hour() + AFTER} {INFO} Copy python script to: {white + script_folder}/{file_name}')
     with open(f'{script_folder}/{file_name}', 'w', encoding='utf-8') as file:
         file.write(script)
     print(f'{BEFORE + current_time_hour() + AFTER} {WAIT} Obfuscation in progress..')
     obfuscated_script = obfuscate(script, size_1, size_2)
     with open(f'{output_folder_1}/{file_name}', 'w', encoding='utf-8') as file:
         file.write(obfuscated_script)
-    print(f'{BEFORE + current_time_hour() + AFTER} {INFO} Obfuscation finished: {white + output_folder_1}/{file_name}')
-
+    print(f'{BEFORE + current_time_hour() + AFTER} {INFO} Obfuscation finish: {white + output_folder_1}/{file_name}')
 while True:
     Zeta_Obfuscator()
     input(f'{BEFORE + current_time_hour() + AFTER} {INPUT} Press to continue.. ')
